8ddb1178e83e5663f42d23c824902aeef3643bf6
FranciscoRauda <raudamejia08@outlook.com>
2023-11-29 11:58:41 -0600

    Añadi lo de blochain

--- src/pages/proyectos/disenio_web/index.jsx
+++ src/pages/proyectos/disenio_web/index.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import web1 from "/public/img/3.png";
 import web2 from "/public/img/Deportes.png";
 import doc from "/public/img/Doc.png";
-import tienda from "/public/img/tienda.jpg";
 import comproyse from "/public/img/comproyse.jpg";
-import web4 from "/public/img/web4.png";  
+import web4 from "/public/img/web4.png";
+import Inicio from "/public/img/INICIO.png";   
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
@@ -13,9 +13,11 @@ import "yet-another-react-lightbox/styles.css";
 
 const index = () => {
   const [open, setOpen] = React.useState(false);
+  const [open0, setOpen0] = React.useState(false);
   const [open2, setOpen2] = React.useState(false);
   const [open3, setOpen3] = React.useState(false);
   const [open4, setOpen4] = React.useState(false);
+  const [open5, setOpen5] = React.useState(false);
   
   return (
     <section className="bg-black h-[70rem] items-center">
@@ -27,19 +29,33 @@ const index = () => {
 
       <div className="max-w-[80rem] mx-auto grid grid-cols-4 px-10 lg:grid-cols-4 items-center py-[10rem] gap-4  ">
 
-      <div className="items-center w-full h-full text-center ">
-          <Image
-            className="object-cover h-full rounded-lg"
-            src={web1}
-           
-            alt="Picture of the author"
-          />
-        </div>
+      
+
+        <button type="button" onClick={() => setOpen0(true)}>
+        <Image
+          src={web1}
+          width={350}
+          height={350}
+          alt="Picture of the author"
+          className="rounded"
+            />
+            
+      </button>
+      
+      <Lightbox
+        open={open0}
+        close={() => setOpen0(false)}
+        slides={[
+          { src: "/img/3.png" },
+          
+          
+        ]}
+      />
 
         <>
         
       <button type="button" onClick={() => setOpen(true)}>
-        Open Lightbox <Image
+      <Image
           src={web2}
           width={350}
           height={350}
@@ -64,7 +80,7 @@ const index = () => {
     </>
         {/* Bloque 2 */}
       <button type="button" onClick={() => setOpen2(true)}>
-        Open Lightbox 2
+    
         <Image
           src={doc}
           width={350}
@@ -135,7 +151,32 @@ const index = () => {
 
     
     
+        {/* Bloque 5  */}
+      
+      <button type="button" onClick={() => setOpen5(true)}>
+         <Image
+          src={Inicio}
+          width={350}
+          height={350}
+          alt="Picture of the author"
+          className="rounded"
+          
+            /> 
+      <h5 className="text-white">Bootcap Blochainverse</h5>
+
+      </button>
+     
+      <Lightbox
+        open={open5}
+        close={() => setOpen5(false)}
+        slides={[
+          { src: "/img/INICIO.PNG" },
+          { src: "/img/panel de ventas.png" },
+          { src: "/img/INVENTARIO.png" },
+          { src: "/img/TIENDA.png" },
+        ]}
         
+        />
         
         
      </div>
